a1d53e9a129c6e52a286c6ded03ca82a4413101f
DeweyLiu <378648742@qq.com>
2018-12-20 10:53:10 +0800

    集成一个eureka客户端实例

---
+++ src/main/java/com/deweyliu/spring/cloud/weather/WeatherEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.deweyliu.spring.cloud.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class WeatherEurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherEurekaClientApplication.class, args);
+    }
+}
